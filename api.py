fc291b9631b12bbb7bf279fa6958435d605cb3f1
Federico <federicoperezramirez@gmail.com>
2025-10-10 23:17:00 -0500

    small changes

--- api.py
+++ api.py
@@ -4,46 +4,75 @@ from theaters_search.theaters.cinepolis import Cinepolis
 from theaters_search.theaters.royal_films import RoyalFilms
 from remove_duplicates.remove_duplicates import group_similar_films
 
-def all_films(city):
-    pass
-
-def search_film(films_map,film,city):
-    pass
-
-
 royal_films = RoyalFilms()
-royal_films.get_films('Bogotá')
-
-cine_col = CineCol()
-cine_col.get_films('Bogota')
-
-cinemark = Cinemark()
-cinemark.get_films('bogota')
-
 cinepolis = Cinepolis()
-cinepolis.get_films('bogota')
- 
-films = {
-    **cine_col.films['Bogota'],
-    **cinemark.films['bogota'],
-    **cinepolis.films['bogota'],
-    **royal_films.films['Bogotá'],
-}
-films = group_similar_films(films)
+cine_col = CineCol()
+cinemark = Cinemark()
+
+def all_films(city):
+    
+    royal_films.get_films('Bogotá')
+    cine_col.get_films('Bogota')    
+    cinemark.get_films('bogota')    
+    cinepolis.get_films('bogota')
+    
+    films = {
+        **cine_col.films['Bogota'],
+        **cinemark.films['bogota'],
+        **cinepolis.films['bogota'],
+        **royal_films.films['Bogotá'],
+    }
+    films = group_similar_films(films)
+    
+    return films
+
+def search_film(films,film,city):
+
+    royal_films.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogotá')
+    royal_films.locations['Bogotá']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito']
+
+    cinepolis.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','bogota')
+    cinepolis.locations['bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo Infinito']
+
+    cinemark.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','bogota')
+    cinemark.locations['bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito']
+
+    cine_col.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogota')
+    cine_col.locations['Bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogota']
+
+# royal_films = RoyalFilms()
+# royal_films.get_films('Bogotá')
+#
+# cine_col = CineCol()
+# cine_col.get_films('Bogota')
+#
+# cinemark = Cinemark()
+# cinemark.get_films('bogota')
+#
+# cinepolis = Cinepolis()
+# cinepolis.get_films('bogota')
+#
+# films = {
+#     **cine_col.films['Bogota'],
+#     **cinemark.films['bogota'],
+#     **cinepolis.films['bogota'],
+#     **royal_films.films['Bogotá'],
+# }
+# films = group_similar_films(films)
 #print(films)
 
-royal_films.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogotá')
-print('ROYAL FILMS')
-print(royal_films.locations['Bogotá']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito'])
-
-cinepolis.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','bogota')
-print('CINEPOLIS')
-print(cinepolis.locations['bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo Infinito'])
-
-cinemark.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','bogota')
-print('CINEMARK')
-print(cinemark.locations['bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito'])
-
-print('CINE COLOMBIA')
-cine_col.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogota')
-print(cine_col.locations['Bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito'])
+# royal_films.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogotá')
+# print('ROYAL FILMS')
+# print(royal_films.locations['Bogotá']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito'])
+#
+# cinepolis.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','bogota')
+# print('CINEPOLIS')
+# print(cinepolis.locations['bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo Infinito'])
+#
+# cinemark.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','bogota')
+# print('CINEMARK')
+# print(cinemark.locations['bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito'])
+#
+# print('CINE COLOMBIA')
+# cine_col.search_showtimes_film(films,'Demon Slayer: Kimetsu no Yaiba - Castillo infinito','Bogota')
+# print(cine_col.locations['Bogota']['Demon Slayer: Kimetsu no Yaiba - Castillo infinito'])
